73d7868ceb174de6860e91d6b33b404ef563d809
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-04-06 22:24:07 -0400

    commit tree c603ac28041e1ca5c8c78a40f9cf624faf6417c9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :sufficiently_clickbaity
+
+    def sufficiently_clickbaity
+        if title
+            unless ["won't believe", "secret", "top", "guess"].any? {|cb| title.downcase.include?(cb)}
+                errors.add(:title, "not clickbaity enough")
+            end
+        end
+    end
+
 end
